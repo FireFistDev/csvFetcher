a4262ae1cfcbd22da76b5d2fa4671c67257ea33c
FireFistDev <akaki7600@gmail.com>
2023-06-21 17:33:06 +0400

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import axios from 'axios';
 import { PrismaService } from './prisma.service';
-import * as  csvParser from 'csv-parser';
+import * as csvParser from 'csv-parser';
 
 @Injectable()
 export class AppService {
@@ -12,42 +12,44 @@ export class AppService {
   async fetchCsvData() {
     try {
       let counter = 0;
-      console.log('start Fetching')
-      const response = await axios.get('https://www.pais.co.il/chance/chance_resultsDownload.aspx', {
-        headers: { Accept: 'text/csv' },
-        responseType: 'stream',
-      });
+      let formerData = []
+      console.log('start Fetching');
+      const response = await axios.get(
+        'https://www.pais.co.il/chance/chance_resultsDownload.aspx',
+        {
+          headers: { Accept: 'text/csv' },
+          responseType: 'stream',
+        },
+      );
       response.data
         .pipe(csvParser(['date', 'id', 'r1', 'r2', 'r3', 'r4']))
         .on('data', async (data) => {
-
-          if (counter === 0){
+          if (counter === 0) {
             counter++;
-            return
-          };
+            return;
+          }
+          counter++;
 
           delete data._6;
-          counter++;
 
           if (counter > 20) {
             response.data.destroy();
             return;
           }
 
-          
-          if (await this.prisma.chance.findFirst({ where: { id: data.id } })) {
-            console.log('thats object alredy existed')
-            return;
-          }
-          const  [day, month, year] = data.date.split('/');
-          const formattedDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
-          console.log(formattedDate, day, month ,year)
-          await this.prisma.chance.create({
-            data: { ...data, date: formattedDate.toISOString() },
+          const [day, month, year] = data.date.split('/');
+          const formattedDate = new Date(
+            parseInt(year),
+            parseInt(month) - 1,
+            parseInt(day),
+          );
+          formerData.push({...data,date:formattedDate})
+          await this.prisma.chance.createMany({
+            data:formerData,
+            skipDuplicates: true, // Skip 'Bobo'
           });
-
         });
-        return 'great job '
+      return 'great job ';
     } catch (error) {
       console.log(error);
     }
