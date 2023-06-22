0a1e85bf48c48d65ea07e303a2cdeabdfc1cb0e0
FireFistDev <akaki7600@gmail.com>
2023-06-22 11:34:32 +0400

    finish project

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import axios from 'axios';
 import { PrismaService } from './prisma.service';
+import * as  moment from 'moment-timezone';
 import * as csvParser from 'csv-parser';
 
 @Injectable()
@@ -11,7 +12,6 @@ export class AppService {
   @Cron(CronExpression.EVERY_10_MINUTES)
   async fetchCsvData() {
     try {
-      let counter = 0;
       let formerData = []
       console.log('start Fetching');
       const response = await axios.get(
@@ -21,34 +21,23 @@ export class AppService {
           responseType: 'stream',
         },
       );
+
       response.data
         .pipe(csvParser(['date', 'id', 'r1', 'r2', 'r3', 'r4']))
         .on('data', async (data) => {
-          if (counter === 0) {
-            counter++;
-            return;
-          }
-          counter++;
-
           delete data._6;
-
-          if (counter > 20) {
-            response.data.destroy();
-            return;
-          }
-
-          const [day, month, year] = data.date.split('/');
-          const formattedDate = new Date(
-            parseInt(year),
-            parseInt(month) - 1,
-            parseInt(day),
-          );
-          formerData.push({...data,date:formattedDate})
-          await this.prisma.chance.createMany({
-            data:formerData,
-            skipDuplicates: true, // Skip 'Bobo'
-          });
-        });
+          formerData.push(data)
+          })
+          .on('end', async()=>{
+            await this.prisma.chance.createMany({
+              data: formerData.slice(1,20).map(elem =>{
+                console.log(elem)
+                return {
+                  ...elem, date: moment.utc(`${elem.date.split('/').reverse().join('-')} 00:00.000`).tz("israel").format()
+                }
+              }),skipDuplicates:true
+            })
+          })
       return 'great job ';
     } catch (error) {
       console.log(error);
